ca17b2ad73dc5c53f4093fb665e060f0d9271407
Makersmarx <bbiehn1@gmail.com>
2020-10-18 15:58:31 -0600

    fix link to linkedin

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -30,7 +30,7 @@ const AboutMe = () => {
           />
           <FaLinkedin
             onClick={() => {
-              window.open('https://www.linkedin.com/in/robert-b-8123891b6');
+              window.open('https://www.linkedin.com/in/makersmarx');
             }}
           />
           <FaJs
