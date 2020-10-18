4d1d169d969d8067a03b9d6820d6139dbfe11031
Makersmarx <bbiehn1@gmail.com>
2020-10-18 14:49:22 -0600

    Updates

---
+++ precache-manifest.d02e6f5cc58a35935f5c0db0bf4c8152.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bda86f6fabc6c997672a9301effee87",
+    "url": "/index.html"
+  },
+  {
+    "revision": "386a7c3eeadc04a7f939",
+    "url": "/static/css/main.4ab4ba55.chunk.css"
+  },
+  {
+    "revision": "82714434e2c01de7cce6",
+    "url": "/static/js/2.36555173.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.36555173.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "386a7c3eeadc04a7f939",
+    "url": "/static/js/main.f8286125.chunk.js"
+  },
+  {
+    "revision": "29ee44826ab01071335c",
+    "url": "/static/js/runtime-main.8704d4bd.js"
+  }
+]);
