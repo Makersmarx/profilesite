71a83404af35a379d19fc8e5dd8db5a910d199ec
Makersmarx <bbiehn1@gmail.com>
2020-10-19 10:46:15 -0600

    typo corrected

--- src/about.js
+++ src/about.js
@@ -1,4 +1,4 @@
-let about = `dates back to the late 80's & 90's. As a kid I was obsessed with tearing old computers apart and rebuilding them. Storing data on an old floppy to look cool in front of my friends! 
+let about = `Dates back to the late 80's & 90's. As a kid I was obsessed with tearing old computers apart and rebuilding them. Storing data on an old floppy to look cool in front of my friends! 
 
 However, somewhere along that path I was side tracked and pursued a career in sales and marketing. I have been blessed during this stage of my life, as it has allowed me to see the world. It has also taught me how to communicate and formulate my ideas into ways that people can understand and openly participate in. However, I was never really happy in this pursuit so I decided to take my passion and rekindle that via school, self-learning, and lots of practice. 
 
